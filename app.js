9a7db979640e152b62930437d8d4e2074d2c478e
sarthak <sarhakbhagat2006@gmail.com>
2025-04-28 16:35:37 +0530

    Deployment2

--- app.js
+++ app.js
@@ -52,9 +52,9 @@ const store = MongoStore.create({
     touchAfter: 24 * 3600,
 })
 
-store.on("error", ()=>{
+store.on("error", (err) => {
     console.log("ERROR IN MONGO SESSION STORE", err);
-})
+});
 
 
 const sessionOption = {
